d78db9b5bca6eb5b3f3e1bbff59ee9001b491024
calliope-net <141074597+calliope-net@users.noreply.github.com>
2023-09-07 17:03:17 +0200

    Aktualisiere pxt.json, main.blocks, main.ts, oled-eeprom.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,9 @@
-
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    oledssd1315.writeText(oledssd1315.oledssd1315_eADDR(oledssd1315.eADDR.OLED_16x8), 0, 2, 15, oledssd1315.eAlign.left, "EEPROM brennen")
+    oledssd1315.writeText(oledssd1315.oledssd1315_eADDR(oledssd1315.eADDR.OLED_16x8), 2, 4, 11, oledssd1315.eAlign.left, "fertig")
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function () {
+    oledssd1315.fillScreen(oledssd1315.oledssd1315_eADDR(oledssd1315.eADDR.OLED_16x8))
+})
+oledssd1315.init(oledssd1315.oledssd1315_eADDR(oledssd1315.eADDR.OLED_16x8))
+oledssd1315.init(oledssd1315.oledssd1315_eADDR(oledssd1315.eADDR.OLED_16x8_x3D))
